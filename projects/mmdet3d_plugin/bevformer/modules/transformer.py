e23c4ae095d00750b9c239d30da48acbda704582
rod409 <radoyeh@gmail.com>
2025-02-06 20:07:40 -0800

    add rotate function that works with tensor input for center -fixes onnx inference accuracy to match pytorch inference on dataset subset

--- projects/mmdet3d_plugin/bevformer/modules/transformer.py
+++ projects/mmdet3d_plugin/bevformer/modules/transformer.py
@@ -15,13 +15,13 @@ from mmdet.models.utils.builder import TRANSFORMER
 from torch.nn.init import normal_
 from projects.mmdet3d_plugin.models.utils.visual import save_tensor
 from mmcv.runner.base_module import BaseModule
-from torchvision.transforms.functional import rotate
+#from torchvision.transforms.functional import rotate
+from ...models.utils import rotate
 from .temporal_self_attention import TemporalSelfAttention
 from .spatial_cross_attention import MSDeformableAttention3D
 from .decoder import CustomMSDeformableAttention
 from projects.mmdet3d_plugin.models.utils.bricks import run_time
 from mmcv.runner import force_fp32, auto_fp16
-
 
 @TRANSFORMER.register_module()
 class PerceptionTransformer(BaseModule):
@@ -167,7 +167,7 @@ class PerceptionTransformer(BaseModule):
                     rotation_angle = can_bus[-1]
                     tmp_prev_bev = prev_bev[:, i].reshape(
                         bev_h, bev_w, -1).permute(2, 0, 1)
-                    tmp_prev_bev = rotate(tmp_prev_bev, rotation_angle.item(),
+                    tmp_prev_bev = rotate(tmp_prev_bev, rotation_angle,
                                           center=self.rotate_center)
                     tmp_prev_bev = tmp_prev_bev.permute(1, 2, 0).reshape(
                         bev_h * bev_w, 1, -1)
---
+++ tools/test_onnx.py
@@ -0,0 +1,374 @@
+# ---------------------------------------------
+# Copyright (c) OpenMMLab. All rights reserved.
+# ---------------------------------------------
+#  Modified by Zhiqi Li
+# ---------------------------------------------
+import sys
+import argparse
+import mmcv
+import os
+import torch
+import torchviz
+import warnings
+from mmcv import Config, DictAction
+from mmcv.cnn import fuse_conv_bn
+from mmcv.parallel import MMDataParallel, MMDistributedDataParallel
+from mmcv.runner import (get_dist_info, init_dist, load_checkpoint, wrap_fp16_model)
+
+from mmdet3d.apis import single_gpu_test
+from mmdet3d.datasets import build_dataset
+from projects.mmdet3d_plugin.datasets.builder import build_dataloader
+from mmdet3d.models import build_model
+from mmdet.apis import set_random_seed
+from projects.mmdet3d_plugin.bevformer.apis.test import custom_multi_gpu_test
+from mmdet.datasets import replace_ImageToTensor
+import time
+import os.path as osp
+
+import onnx
+import onnxruntime as ort
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description='MMDet test (and eval) a model')
+    parser.add_argument('config', help='test config file path')
+    parser.add_argument('checkpoint', help='checkpoint file')
+    parser.add_argument('--out', help='output result file in pickle format')
+    parser.add_argument(
+        '--fuse-conv-bn',
+        action='store_true',
+        help='Whether to fuse conv and bn, this will slightly increase'
+        'the inference speed')
+    parser.add_argument(
+        '--format-only',
+        action='store_true',
+        help='Format the output results without perform evaluation. It is'
+        'useful when you want to format the result to a specific format and '
+        'submit it to the test server')
+    parser.add_argument(
+        '--eval',
+        type=str,
+        nargs='+',
+        help='evaluation metrics, which depends on the dataset, e.g., "bbox",'
+        ' "segm", "proposal" for COCO, and "mAP", "recall" for PASCAL VOC')
+    parser.add_argument('--show', action='store_true', help='show results')
+    parser.add_argument(
+        '--show-dir', help='directory where results will be saved')
+    parser.add_argument(
+        '--gpu-collect',
+        action='store_true',
+        help='whether to use gpu to collect results.')
+    parser.add_argument(
+        '--tmpdir',
+        help='tmp directory used for collecting results from multiple '
+        'workers, available when gpu-collect is not specified')
+    parser.add_argument('--seed', type=int, default=0, help='random seed')
+    parser.add_argument(
+        '--deterministic',
+        action='store_true',
+        help='whether to set deterministic options for CUDNN backend.')
+    parser.add_argument(
+        '--cfg-options',
+        nargs='+',
+        action=DictAction,
+        help='override some settings in the used config, the key-value pair '
+        'in xxx=yyy format will be merged into config file. If the value to '
+        'be overwritten is a list, it should be like key="[a,b]" or key=a,b '
+        'It also allows nested list/tuple values, e.g. key="[(a,b),(c,d)]" '
+        'Note that the quotation marks are necessary and that no white space '
+        'is allowed.')
+    parser.add_argument(
+        '--options',
+        nargs='+',
+        action=DictAction,
+        help='custom options for evaluation, the key-value pair in xxx=yyy '
+        'format will be kwargs for dataset.evaluate() function (deprecate), '
+        'change to --eval-options instead.')
+    parser.add_argument(
+        '--eval-options',
+        nargs='+',
+        action=DictAction,
+        help='custom options for evaluation, the key-value pair in xxx=yyy '
+        'format will be kwargs for dataset.evaluate() function')
+    parser.add_argument(
+        '--launcher',
+        choices=['none', 'pytorch', 'slurm', 'mpi'],
+        default='none',
+        help='job launcher')
+    parser.add_argument('--local_rank', type=int, default=0)
+    args = parser.parse_args()
+    if 'LOCAL_RANK' not in os.environ:
+        os.environ['LOCAL_RANK'] = str(args.local_rank)
+
+    if args.options and args.eval_options:
+        raise ValueError(
+            '--options and --eval-options cannot be both specified, '
+            '--options is deprecated in favor of --eval-options')
+    if args.options:
+        warnings.warn('--options is deprecated in favor of --eval-options')
+        args.eval_options = args.options
+    return args
+
+
+def to_numpy(tensor):
+    return tensor.detach().cpu().numpy() if tensor.requires_grad else tensor.cpu().numpy()
+
+def main():
+    args = parse_args()
+
+    assert args.out or args.eval or args.format_only or args.show \
+        or args.show_dir, \
+        ('Please specify at least one operation (save/eval/format/show the '
+         'results / save the results) with the argument "--out", "--eval"'
+         ', "--format-only", "--show" or "--show-dir"')
+
+    if args.eval and args.format_only:
+        raise ValueError('--eval and --format_only cannot be both specified')
+
+    if args.out is not None and not args.out.endswith(('.pkl', '.pickle')):
+        raise ValueError('The output file must be a pkl file.')
+
+    cfg = Config.fromfile(args.config)
+    if args.cfg_options is not None:
+        cfg.merge_from_dict(args.cfg_options)
+    # import modules from string list.
+    if cfg.get('custom_imports', None):
+        from mmcv.utils import import_modules_from_strings
+        import_modules_from_strings(**cfg['custom_imports'])
+
+    # import modules from plguin/xx, registry will be updated
+    if hasattr(cfg, 'plugin'):
+        if cfg.plugin:
+            import importlib
+            if hasattr(cfg, 'plugin_dir'):
+                plugin_dir = cfg.plugin_dir
+                _module_dir = os.path.dirname(plugin_dir)
+                _module_dir = _module_dir.split('/')
+                _module_path = _module_dir[0]
+
+                for m in _module_dir[1:]:
+                    _module_path = _module_path + '.' + m
+                print(_module_path)
+                plg_lib = importlib.import_module(_module_path)
+            else:
+                # import dir is the dirpath for the config file
+                _module_dir = os.path.dirname(args.config)
+                _module_dir = _module_dir.split('/')
+                _module_path = _module_dir[0]
+                for m in _module_dir[1:]:
+                    _module_path = _module_path + '.' + m
+                print(_module_path)
+                plg_lib = importlib.import_module(_module_path)
+
+    # build the model and load checkpoint
+    '''cfg.model.train_cfg = None
+    model = build_model(cfg.model, test_cfg=cfg.get('test_cfg'))
+    fp16_cfg = cfg.get('fp16', None)
+    if fp16_cfg is not None:
+        wrap_fp16_model(model)
+    checkpoint = load_checkpoint(model, args.checkpoint, map_location='cpu')
+    if args.fuse_conv_bn:
+        model = fuse_conv_bn(model)'''
+
+    #model.eval()
+    # rand_example = torch.rand(6, 3, 900, 1600, dtype=torch.float32)
+    # traced_script_module = torch.jit.trace(model.encoders.camera.backbone, rand_example, strict=False)
+    # traced_script_module.save('traced_model_encoders_camera.pt')
+
+    # set cudnn_benchmark
+    if cfg.get('cudnn_benchmark', False):
+        torch.backends.cudnn.benchmark = True
+    # set tf32
+    if cfg.get('close_tf32', False):
+        torch.backends.cuda.matmul.allow_tf32 = False
+        torch.backends.cudnn.allow_tf32 = False
+
+    cfg.model.pretrained = None
+    # in case the test dataset is concatenated
+    samples_per_gpu = 1
+    if isinstance(cfg.data.test, dict):
+        cfg.data.test.test_mode = True
+        samples_per_gpu = cfg.data.test.pop('samples_per_gpu', 1)
+        if samples_per_gpu > 1:
+            # Replace 'ImageToTensor' to 'DefaultFormatBundle'
+            cfg.data.test.pipeline = replace_ImageToTensor(
+                cfg.data.test.pipeline)
+    elif isinstance(cfg.data.test, list):
+        for ds_cfg in cfg.data.test:
+            ds_cfg.test_mode = True
+        samples_per_gpu = max(
+            [ds_cfg.pop('samples_per_gpu', 1) for ds_cfg in cfg.data.test])
+        if samples_per_gpu > 1:
+            for ds_cfg in cfg.data.test:
+                ds_cfg.pipeline = replace_ImageToTensor(ds_cfg.pipeline)
+
+    # init distributed env first, since logger depends on the dist info.
+    if args.launcher == 'none':
+        distributed = False
+    else:
+        distributed = True
+        init_dist(args.launcher, **cfg.dist_params)
+
+    # set random seeds
+    if args.seed is not None:
+        set_random_seed(args.seed, deterministic=args.deterministic)
+
+    # build the dataloader
+    dataset = build_dataset(cfg.data.test)
+    # from projects.mmdet3d_plugin.datasets.nuscenes_dataset import CustomNuScenesDataset
+    # dataset = CustomNuScenesDataset(**cfg.data.test.copy())
+    data_loader = build_dataloader(
+        dataset,
+        samples_per_gpu=samples_per_gpu,
+        #workers_per_gpu=cfg.data.workers_per_gpu,
+        workers_per_gpu=0,
+        dist=distributed,
+        shuffle=False,
+        nonshuffler_sampler=cfg.data.nonshuffler_sampler,
+    )
+
+    # build the model and load checkpoint
+    cfg.model.train_cfg = None
+    model = build_model(cfg.model, test_cfg=cfg.get('test_cfg'))
+    fp16_cfg = cfg.get('fp16', None)
+    if fp16_cfg is not None:
+        wrap_fp16_model(model)
+    checkpoint = load_checkpoint(model, args.checkpoint, map_location='cpu')
+    if args.fuse_conv_bn:
+        model = fuse_conv_bn(model)
+
+    # old versions did not save class info in checkpoints, this walkaround is
+    # for backward compatibility
+    if 'CLASSES' in checkpoint.get('meta', {}):
+        model.CLASSES = checkpoint['meta']['CLASSES']
+    else:
+        model.CLASSES = dataset.CLASSES
+    # palette for visualization in segmentation tasks
+    if 'PALETTE' in checkpoint.get('meta', {}):
+        model.PALETTE = checkpoint['meta']['PALETTE']
+    elif hasattr(dataset, 'PALETTE'):
+        # segmentation dataset has `PALETTE` attribute
+        model.PALETTE = dataset.PALETTE
+
+    modules = dict()
+    for k, m in model.named_modules():
+        for c1, c2 in m.named_children():
+            '''if c2._get_name() ==  'ModulatedDeformConv2dPack':
+                c3 = torch.nn.Conv2d(in_channels=c2.in_channels, out_channels=c2.out_channels, kernel_size=c2.kernel_size, stride=c2.stride, padding=c2.padding, dilation=c2.dilation, groups=c2.groups, bias=c2.bias)
+                c3.weight = c2.weight
+                c3.bias = c2.bias
+                c3.output_padding = c2.output_padding
+                setattr(m, c1, c3)'''
+            '''if c2._get_name() == 'MSDeformableAttention3D':
+                c3 = torch.nn.MultiheadAttention(c2.embed_dims, c2.num_heads, batch_first=c2.batch_first)
+                setattr(m, c1, c3)'''
+            '''if c2._get_name() == 'CustomMSDeformableAttention':
+                c3 = torch.nn.MultiheadAttention(c2.embed_dims, c2.num_heads, batch_first=c2.batch_first)
+                setattr(m, c1, c3)'''
+
+    for k, m2 in modules.items():
+        import re
+        search = re.search('\.\d+\.', k)
+        if search is not None:
+            eval('model.' + k[:search.start(0)] + '[' + k[search.start(0)+1 : search.end(0)-1] + ']').__setattr__(k[search.end(0):], m2)
+
+    if not distributed:
+        # assert False
+        model = MMDataParallel(model, device_ids=[0])
+        ort_sess = ort.InferenceSession('/home/rod/Documents/research/BEVFormer/bevformer.onnx')
+        input_img_name = ort_sess.get_inputs()[0].name
+        input_prev_bev_name = ort_sess.get_inputs()[1].name
+        input_use_prev_bev_name = ort_sess.get_inputs()[2].name
+        input_can_bus_name = ort_sess.get_inputs()[3].name
+        input_lidar2img_name = ort_sess.get_inputs()[4].name
+        model.eval()
+        results = []
+        prev_frame_info = {
+            "scene_token": None,
+            "prev_pos": 0,
+            "prev_angle": 0,
+        }
+        prev_bev = torch.zeros(cfg.bev_h_ * cfg.bev_w_, 1, cfg._dim_)
+        #prev_bev = None
+        dataset = data_loader.dataset
+        prog_bar = mmcv.ProgressBar(len(dataset))
+        for i, data in enumerate(data_loader):
+            with torch.no_grad():
+                img_metas = data["img_metas"][0].data[0]
+                tmp_pos = (img_metas[0]['can_bus'][:3]).clone()
+                tmp_angle = (img_metas[0]['can_bus'][-1]).clone()
+                if img_metas[0]["scene_token"] != prev_frame_info["scene_token"]:
+                    use_prev_bev = torch.tensor(0.0)
+                    #prev_bev = None
+                    img_metas[0]["can_bus"][-1] = 0
+                    img_metas[0]["can_bus"][:3] = 0
+                else: 
+                    use_prev_bev = torch.tensor(1.0)
+                    img_metas[0]["can_bus"][:3] -= prev_frame_info["prev_pos"]
+                    img_metas[0]["can_bus"][-1] -= prev_frame_info["prev_angle"]
+                prev_frame_info["scene_token"] = img_metas[0]["scene_token"]
+                '''if i == 0:
+                    img = data["img"][0].data[0]
+                    image_metas = []
+                    for i in range(len(img_metas)):
+                        lidar2img = [l for l in img_metas[i]['lidar2img']]
+                        #img_shape = [torch.from_numpy(s) for s in kwargs['img_metas'][i]['img_shape']]
+                        image_metas.append({'scene_token': img_metas[i]['scene_token'],'lidar2img': lidar2img, 'img_shape': torch.tensor(img_metas[i]['img_shape']), 'can_bus': img_metas[i]['can_bus']})
+                    torch.onnx.export(model.module, (False, prev_bev,  use_prev_bev, [image_metas], [img]), 'bevformer.onnx', verbose=True, opset_version=16, dynamic_axes=None)'''
+                can_bus = img_metas[0]["can_bus"].to(torch.float32)
+                lidar2img = torch.stack(img_metas[0]['lidar2img']).unsqueeze(0).to(torch.float32)
+                img = data["img"][0].data[0]
+                input_data = {input_img_name: to_numpy(img),
+                    input_prev_bev_name: to_numpy(prev_bev),
+                    input_use_prev_bev_name: to_numpy(use_prev_bev),
+                    input_can_bus_name: to_numpy(can_bus),
+                    input_lidar2img_name: to_numpy(lidar2img),
+                    }
+                result = ort_sess.run(None, input_data)
+                bev_embed = torch.from_numpy(result[0])
+                outputs_classes = torch.from_numpy(result[1])
+                outputs_coords = torch.from_numpy(result[2])
+                result = model.module.post_process(outputs_classes, outputs_coords, img_metas)
+                results.extend(result)
+                prev_bev = bev_embed
+                prev_frame_info["prev_pos"] = tmp_pos
+                prev_frame_info["prev_angle"] = tmp_angle
+            batch_size = len(result)
+            for _ in range(batch_size):
+                prog_bar.update()
+        outputs = results
+    else:
+        model = MMDistributedDataParallel(
+            model.cuda(),
+            device_ids=[torch.cuda.current_device()],
+            broadcast_buffers=False)
+        outputs = custom_multi_gpu_test(model, data_loader, args.tmpdir,
+                                        args.gpu_collect)
+
+    rank, _ = get_dist_info()
+    if rank == 0:
+        if args.out:
+            print(f'\nwriting results to {args.out}')
+            assert False
+            #mmcv.dump(outputs['bbox_results'], args.out)
+        kwargs = {} if args.eval_options is None else args.eval_options
+        kwargs['jsonfile_prefix'] = osp.join('test', args.config.split(
+            '/')[-1].split('.')[-2], time.ctime().replace(' ', '_').replace(':', '_'))
+        if args.format_only:
+            dataset.format_results(outputs, **kwargs)
+
+        if args.eval:
+            eval_kwargs = cfg.get('evaluation', {}).copy()
+            # hard-code way to remove EvalHook args
+            for key in [
+                    'interval', 'tmpdir', 'start', 'gpu_collect', 'save_best',
+                    'rule'
+            ]:
+                eval_kwargs.pop(key, None)
+            eval_kwargs.update(dict(metric=args.eval, **kwargs))
+
+            print(dataset.evaluate(outputs, **eval_kwargs))
+
+
+if __name__ == '__main__':
+    main()
